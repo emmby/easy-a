4fe042eca3334a25638166520aaf710bd620e3ec
Mike Burton <mburton@groupon.com>
2015-02-04 15:45:23 -0800

    try to remove ondestroy bullshit

--- Tasks/src/main/java/com/dummies/tasks/fragment/TaskListFragment.java
+++ Tasks/src/main/java/com/dummies/tasks/fragment/TaskListFragment.java
@@ -1,5 +1,7 @@
 package com.dummies.tasks.fragment;
 
+import android.app.Activity;
+import android.app.Application;
 import android.app.Fragment;
 import android.content.Intent;
 import android.database.Cursor;
@@ -19,22 +21,23 @@ import com.dummies.tasks.activity.PreferencesActivity;
 import com.dummies.tasks.adapter.TaskListAdapter;
 import com.dummies.tasks.interfaces.OnEditTask;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
 import rx.Observable;
 import rx.Subscription;
-import rx.android.app.AppObservable;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.observers.Subscribers;
 import rx.schedulers.Schedulers;
 
 public class TaskListFragment extends Fragment
 {
-    private static final String DATABASE_NAME = "/data/data/com.dummies.tasks/databases/data";
+    private static final String DATABASE_NAME = "data";
     private static final String DATABASE_TABLE = "tasks";
 
     TaskListAdapter adapter;
     RecyclerView recyclerView;
     SQLiteDatabase db;
-    Subscription dbSubscription;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -42,8 +45,13 @@ public class TaskListFragment extends Fragment
 
         adapter = new TaskListAdapter();
         db = SQLiteDatabase.openDatabase(
-            DATABASE_NAME, null,
+            getActivity().getDatabasePath(DATABASE_NAME).getPath(), null,
             SQLiteDatabase.OPEN_READWRITE);
+        
+        // TODO this results in multiple registrations
+        getActivity().getApplication().registerActivityLifecycleCallbacks(
+            RxActivityLifecycleCallbacks.INSTANCE
+        );
     }
 
     @Override
@@ -51,20 +59,22 @@ public class TaskListFragment extends Fragment
         super.onActivityCreated(savedInstanceState);
         setHasOptionsMenu(true);
 
-        dbSubscription = AppObservable.bindFragment(
-            this,
+        RxActivityLifecycleCallbacks.registerSubscriptionForRemoveOnDestroy(
+            getActivity(), 
             Observable.defer(
-                () -> query(db,false,DATABASE_TABLE,null,null,null,null,
-                        null,null,null))
+                () -> query(
+                    db, false, DATABASE_TABLE, null, null, null, null,
+                    null, null, null))
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-            )
-            .subscribe(
-                Subscribers.create(
-                    cursor -> adapter.swapCursor(cursor)
-                ));
+                .subscribe(
+                    Subscribers.create(
+                        cursor -> adapter.swapCursor(cursor)
+                    ))
+        );
+
     }
-    
+
     Observable<Cursor> query( SQLiteDatabase db,
                               boolean distinct,
                               String table,
@@ -78,12 +88,6 @@ public class TaskListFragment extends Fragment
     {
         return Observable.just(db.query(distinct, table, columns, selection,
                 selectionArgs, groupBy, having, orderBy, limit));
-    }
-
-    @Override
-    public void onDestroy() {
-        dbSubscription.unsubscribe();
-        super.onDestroy();        
     }
 
     @Override
@@ -125,3 +129,57 @@ public class TaskListFragment extends Fragment
     }
 }
 
+// TODO This solution won't work with fragments
+class RxActivityLifecycleCallbacks 
+    implements Application.ActivityLifecycleCallbacks 
+{
+    public static final RxActivityLifecycleCallbacks INSTANCE
+        = new RxActivityLifecycleCallbacks();
+    
+    HashMap<Activity, HashSet<Subscription>> map = new HashMap<>();
+    
+    @Override
+    public void onActivityCreated(Activity activity, Bundle 
+        savedInstanceState) {
+    }
+
+    @Override
+    public void onActivityStarted(Activity activity) {
+    }
+
+    @Override
+    public void onActivityResumed(Activity activity) {
+    }
+
+    @Override
+    public void onActivityPaused(Activity activity) {
+    }
+
+    @Override
+    public void onActivityStopped(Activity activity) {
+    }
+
+    @Override
+    public void onActivitySaveInstanceState(Activity activity, Bundle 
+        outState) {
+    }
+
+    @Override
+    public void onActivityDestroyed(Activity activity) {
+        // Remove any outstanding subscriptions
+        HashSet<Subscription> subscriptions = map.remove(activity);
+        for( Subscription subscription : subscriptions )
+            subscription.unsubscribe();
+    }
+
+    public static void registerSubscriptionForRemoveOnDestroy(
+        Activity activity, Subscription subscription) 
+    {
+        // TODO not thread safe and not pretty
+        HashSet<Subscription> subscriptions = INSTANCE.map.get(activity);
+        if( subscriptions==null )
+            subscriptions=new HashSet<>();
+        subscriptions.add(subscription);
+        INSTANCE.map.put(activity,subscriptions);
+    }
+} 
