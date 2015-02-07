6d0632f9803835529fc0d88536f8b5c712489a5e
Mike Burton <mburton@groupon.com>
2015-02-06 18:16:12 -0800

    rename lxglifecycle to lxglifecycle to ensure i'm not using the wrong version of the plugin

--- loglifecycle/loglifecycle-plugin/src/main/groovy/com/github/stephanenicolas/loglifecycle/LogLifeCyclePluginExtension.groovy
+++
@@ -1,9 +0,0 @@
-package com.github.stephanenicolas.loglifecycle
-
-/**
- * @author SNI
- */
-public class LogLifeCyclePluginExtension {
-   /** Enable to debug the lib's errors. */
-   boolean debug;
-}
---
+++ lxglifecycle/lxglifecycle-plugin/src/main/java/com/github/stephanenicolas/lxglifecycle/LxgLifeCycleProcessor.java
@@ -0,0 +1,128 @@
+package com.github.stephanenicolas.lxglifecycle;
+
+import com.github.stephanenicolas.afterburner.AfterBurner;
+import com.github.stephanenicolas.afterburner.exception.AfterBurnerImpossibleException;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+import javassist.CannotCompileException;
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtMethod;
+import javassist.NotFoundException;
+import javassist.build.IClassTransformer;
+import javassist.build.JavassistBuildException;
+import javassist.bytecode.AccessFlag;
+import lombok.extern.slf4j.Slf4j;
+
+import static com.github.stephanenicolas.morpheus.commons.JavassistUtils.isActivity;
+import static com.github.stephanenicolas.morpheus.commons.JavassistUtils.isFragment;
+import static com.github.stephanenicolas.morpheus.commons.JavassistUtils.isSupportFragment;
+
+/**
+ * A class transformer to inject logging byte code for all life cycle methods.
+ *
+ * @author SNI
+ */
+@Slf4j
+public class LxgLifeCycleProcessor implements IClassTransformer {
+    private static final HashSet<String> supportedMethods = new HashSet<>(
+      Arrays.asList("onStart", "onResume", "onPause", "onStop", "onDestroy")
+  );
+  private AfterBurner afterBurner = new AfterBurner();
+  private boolean debug;
+
+    public LxgLifeCycleProcessor(boolean debug) {
+    this.debug = debug;
+  }
+
+  @Override
+  public boolean shouldTransform(CtClass candidateClass) throws JavassistBuildException {
+    try {
+        return isSupported(candidateClass);
+    } catch (Exception e) {
+      logMoreIfDebug("Should transform filter failed for class " + candidateClass.getName(), e);
+      throw new JavassistBuildException(e);
+    }
+  }
+
+  private boolean isSupported(CtClass candidateClass) throws NotFoundException {
+    return isActivity(candidateClass)
+        || isFragment(candidateClass)
+        || isSupportFragment(candidateClass);
+  }
+
+  @Override
+  public void applyTransformations(CtClass classToTransform) throws JavassistBuildException {
+    String classToTransformName = classToTransform.getName();
+    try {
+      log.info("Transforming " + classToTransformName);
+      ClassPool pool = classToTransform.getClassPool();
+      Set<CtMethod> methodSet = getAllLifeCycleMethods(pool, classToTransform.getName());
+      debugLifeCycleMethods(classToTransform, methodSet.toArray(new CtMethod[methodSet.size()]));
+    } catch (Exception e) {
+      logMoreIfDebug("Transformation failed for class " + classToTransformName, e);
+      throw new JavassistBuildException(e);
+    }
+    log.info("Transformation successful for " + classToTransformName);
+  }
+
+  private Set<CtMethod> getAllLifeCycleMethods(ClassPool pool, String className)
+      throws NotFoundException {
+    Set<CtMethod> methodSet = new HashSet<>();
+    CtMethod[] inheritedMethods = pool.get(className).getMethods();
+    CtMethod[] declaredMethods = pool.get(className).getDeclaredMethods();
+    for (CtMethod method : inheritedMethods) {
+      methodSet.add(method);
+    }
+    for (CtMethod method : declaredMethods) {
+      methodSet.add(method);
+    }
+    return methodSet;
+  }
+
+  private void debugLifeCycleMethods(CtClass classToTransform, CtMethod[] methods)
+      throws CannotCompileException, AfterBurnerImpossibleException, NotFoundException {
+    for (CtMethod lifeCycleHook : methods) {
+      String methodName = lifeCycleHook.getName();
+
+      int accessFlags = lifeCycleHook.getMethodInfo().getAccessFlags();
+      boolean isFinal = (accessFlags & AccessFlag.FINAL) == AccessFlag.FINAL;
+      boolean canOverride = !isFinal && (AccessFlag.isPublic(accessFlags)
+          || AccessFlag.isProtected(accessFlags)
+          || AccessFlag.isPackage(accessFlags));
+        
+        log.info("Method name: " + methodName);
+
+        if(!supportedMethods.contains(methodName))
+            continue;
+
+
+        if (canOverride && methodName.startsWith("on")) {
+        log.info("Overriding " + methodName);
+        try {
+         String body = "com.github.stephanenicolas.lxglifecycle.ActivityListenerUtil.internal._" + methodName + "(this);";
+            
+          afterBurner.afterOverrideMethod(classToTransform, methodName, body);
+          log.info("Override successful " + methodName);
+        } catch (Exception e) {
+          logMoreIfDebug("Override didn't work ", e);
+        }
+      } else {
+        log.info(
+            "Skipping " + methodName + ". Either it is final, private or doesn't start by 'on...'");
+      }
+    }
+  }
+
+  private void logMoreIfDebug(String message, Exception e) {
+    if (debug) {
+      log.debug(message, e);
+    } else {
+      log.info(message);
+    }
+  }
+}
